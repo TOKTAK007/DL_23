f9de381f282a111419c09d4a8c89e2ad41f9aab9
PongthepGeo <pongtep_tong@hotmail.com>
2023-01-15 18:43:03 +0700

    Jan_15

--- 11_preparing_image_classification.py
+++ 11_preparing_image_classification.py
@@ -6,11 +6,17 @@ import class_functions as C
 #-----------------------------------------------------------------------------------------#
 import numpy as np
 import matplotlib.pyplot as plt
-import torch
-#-----------------------------------------------------------------------------------------#
-from sklearn.model_selection import train_test_split
-from torch import nn
-#-----------------------------------------------------------------------------------------#
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using PyTorch version:', torch.__version__, ' Device:', device)
-#-----------------------------------------------------------------------------------------#
+import skimage.io as skio
+# # import torch
+# #-----------------------------------------------------------------------------------------#
+# # from sklearn.model_selection import train_test_split
+# from torch import nn
+# #-----------------------------------------------------------------------------------------#
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# print('Using PyTorch version:', torch.__version__, ' Device:', device)
+# #-----------------------------------------------------------------------------------------#
+
+beforeflood = skio.imread('../datasets/satellite_imagery/beforeflood_kmeanB4B3B2.tif', plugin='tifffile')
+flood = skio.imread('../datasets/satellite_imagery/flood_kmeanB4B3B2.tif', plugin='tifffile')
+F.composite_bands(beforeflood, 0.9)
+F.composite_bands(flood, 0.9)
